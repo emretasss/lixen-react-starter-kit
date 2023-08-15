fd0d2870fce1b9adda2b7776f9423d85824ea933
emre <emre@emre-MBP.home>
2023-08-15 05:37:43 +0300

    useEfect and usestate

--- src/Test.js
+++ src/Test.js
@@ -1,14 +1,25 @@
 
-import { useEffect } from "react"
+import { useEffect,useState } from "react"
 
 
  function Test ()
-{useEffect(() => {console.log("compenent ilk yüklendiğinde")},[])
+{
+    const [count,setCount]=useState(0)
+useEffect(() => {console.log("compenent ilk yüklendiğinde")
 
-useEffect(() => {console.log("compenent render oldu")})
+return()=>{console.log("destroy")}
+
+},[])
+
+useEffect(() => {console.log("compenent render oldu")
+
+})
 
     return(
 <div className="App">
+    <h2>{count}</h2>
+        <button onClick={()=> setCount( count=>count +1) } >arttır</button>
+
     test compenent yapı.
 </div>
 
