2f8b305b14f619e6013f5030d0535d9947ab43a5
emre <emre@emre-MBP.home>
2023-08-16 04:45:51 +0300

    fetch posts

--- src/Test.js
+++ src/Test.js
@@ -4,12 +4,28 @@ import { useEffect,useState } from "react"
 
  function Test ()
 {
+    const[posta,postb]=useState(false)
+    const [post,setpost]=useState(1)
+
+
+
     const [count,setCount]=useState(0)
-useEffect(() => {console.log("compenent ilk yüklendiğinde")
+    useEffect(() => {console.log("compenent ilk yüklendiğinde")
+    
+    return()=>{console.log("destroy")}
+    
+    },[])
 
-return()=>{console.log("destroy")}
 
-},[])
+
+useEffect(() => {
+
+
+fetch(`https://jsonplaceholder.typicode.com/posts/${post}`)
+.then(res=> res.json())
+.then(data =>postb(data))
+
+},[post])
 
 useEffect(() => {console.log("compenent render oldu")
 
@@ -20,11 +36,18 @@ useEffect(() => {console.log("compenent render oldu")
     <h2>{count}</h2>
         <button onClick={()=> setCount( count=>count +1) } >arttır</button>
 
+        <h2>{post}</h2>
+        {posta && JSON.stringify(posta)}
+        <button onClick={()=> setpost( post=>post +1) } >diğer konuyu getir</button>
+
     test compenent yapı.
 </div>
 
 
     )
-}
+    }
+
+
+
 
 export default Test
